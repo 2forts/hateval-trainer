587f289a98ecbe13a1abda84b525a67378297b33
Francisco José Orts Gómez <2forts@gmail.com>
2025-09-15 20:26:41 +0200

    Create download_nltk.py

---
+++ scripts/download_nltk.py
@@ -0,0 +1,5 @@
+"""Small helper to pre-download NLTK resources non-interactively."""
+import nltk
+for pkg in ("stopwords", "wordnet"):
+nltk.download(pkg, quiet=True)
+print("NLTK resources downloaded.")
